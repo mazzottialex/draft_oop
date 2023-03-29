92d305af8c137803e60a1977e0543706f88d38da
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-03-29 18:22:22 +0200

    classe calciatore e archivio di questi

---
+++ src/manageData/Calciatore.java
@@ -0,0 +1,30 @@
+package manageData;
+
+public class Calciatore {
+	private String nominativo;
+	private String ruolo;
+	private String squadra;
+	private int pg; //partite giocate
+	private int gol;
+	private int ammonizioni;
+	private int espulsioni;
+	private float mv; //media voti
+	public Calciatore(String nominativo, String ruolo, String squadra, int pg, int gol, int ammonizioni, int espulsioni,
+			float mv) {
+		this.nominativo = nominativo;
+		this.ruolo = ruolo;
+		this.squadra = squadra;
+		this.pg = pg;
+		this.gol = gol;
+		this.ammonizioni = ammonizioni;
+		this.espulsioni = espulsioni;
+		this.mv = mv;
+	}
+	@Override
+	public String toString() {
+		return "Calciatore [nominativo=" + nominativo + ", ruolo=" + ruolo + ", squadra=" + squadra + ", pg=" + pg
+				+ ", gol=" + gol + ", ammonizioni=" + ammonizioni + ", espulsioni=" + espulsioni + ", mv=" + mv + "]";
+	}
+	
+	
+}
--- src/manageData/Scraping.java
+++ src/manageData/Scraping.java
@@ -13,11 +13,11 @@ public class Scraping {
 	private String url="https://www.gazzetta.it/calcio/fantanews/statistiche/serie-a-2022-23/";
 	private Document doc;
 	private Elements el;
-	private List<String> calciatori;
+	private List<Calciatore> li;
 	
 	public Scraping() {
 		
-		calciatori=new ArrayList<>();
+		li=new ArrayList<>();
 		
 		try {
 			doc=Jsoup.connect(url).get();
@@ -25,21 +25,49 @@ public class Scraping {
 			throw new RuntimeException(e); 
 		}
 		
-		el=doc.select("td.field-giocatore");
-		
+		el=doc.select("tr");
 		for(Element e : el) {
-			calciatori.add(e.selectFirst("a").text());
+			if(e.select("td.field-giocatore").text()!="")
+			li.add(new Calciatore(
+					e.select("td.field-giocatore").text(),
+					e.select("td.field-ruolo").text(),
+					e.select("td.field-sqd").text(),
+					ParseInt(e.select("td.field-pg").text()),
+					ParseInt(e.select("td.field-g").text()),
+					ParseInt(e.select("td.field-am").text()),
+					ParseInt(e.select("td.field-es").text()),
+					ParseFloat(e.select("td.field-mv").text())));
 		}
 	}
 	
-	public List<String> getCalciatori() {
-		return calciatori;
+	private int ParseInt(String s) {
+		int ret;
+		try {
+			ret=Integer.parseInt(s);
+		}
+		catch(Exception e) {
+			ret=0;
+		}
+		return ret;
+	}
+	
+	private float ParseFloat(String s) {
+		float ret;
+		try {
+			ret=Float.parseFloat(s);
+		}
+		catch(Exception e) {
+			ret=(float) 0.0;
+		}
+		return ret;
+	}
+	
+	public Calciatore getCalciatori() {
+		return li.get(400);
 	}
 
-	public void setCalciatori(List<String> calciatori) {
-		this.calciatori = calciatori;
-	}
 
+	
 	public Document getDoc() {
 		return doc;
 	}
