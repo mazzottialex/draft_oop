f02efef91a55ef66108a8aafacce8111e2873c30
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-04-18 18:03:48 +0200

    Aggiunti elementi alla gui

--- src/gui/HomeImpl.java
+++ src/gui/HomeImpl.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JList;
 
 public class HomeImpl extends JFrame implements Home   {
 
@@ -36,6 +40,39 @@ public class HomeImpl extends JFrame implements Home   {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JLabel lblDraft = new JLabel("DRAFT");
+		contentPane.add(lblDraft);
+		
+		JButton btnStart = new JButton("INIZIO PARTITA");
+		contentPane.add(btnStart);
+		
+		JPanel panelSelectioned = new JPanel();
+		contentPane.add(panelSelectioned);
+		
+		JLabel lblStagioneSelezionata = new JLabel("Stagione selezionata:");
+		panelSelectioned.add(lblStagioneSelezionata);
+		
+		JLabel lblStagione = new JLabel("2022-23");
+		panelSelectioned.add(lblStagione);
+		
+		JPanel panelLoad = new JPanel();
+		contentPane.add(panelLoad);
+		
+		JLabel lblCarica = new JLabel("Carica stagione:");
+		panelLoad.add(lblCarica);
+		
+		JList listCarica = new JList();
+		panelLoad.add(listCarica);
+		
+		JPanel panelDownload = new JPanel();
+		contentPane.add(panelDownload);
+		
+		JLabel lblAggiorna = new JLabel("Aggiorna stagione:");
+		panelDownload.add(lblAggiorna);
+		
+		JList listAggiorna = new JList();
+		panelDownload.add(listAggiorna);
 	}
 
 }
