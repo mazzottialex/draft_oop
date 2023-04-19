a015019377a3aa15a26e6e873bb372d5cebabb13
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-04-19 16:35:17 +0200

    LogicsHomeImpl caricamento/download dati

--- src/logics/LogicsHome.java
+++ src/logics/LogicsHome.java
@@ -1,12 +1,17 @@
 package logics;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 import data.Calciatore;
 
 public interface LogicsHome {
+	public String getStagione();
+	public void setStagione(String stagione);
 	public List<String> getStagioni();
 	public Boolean loadStagione(String stagione);
 	public Boolean downloadStagione(String stagione);
-	public List<Calciatore> geLi();
+	public List<Calciatore> getLi() throws FileNotFoundException, ClassNotFoundException, IOException;
+	
 }
--- src/logics/LogicsHomeImpl.java
+++ src/logics/LogicsHomeImpl.java
@@ -1,33 +1,80 @@
 package logics;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import data.Calciatore;
+import manageData.ManageData;
+import manageData.ManageDataImpl;
+import rating.CalcoloRating;
+import rating.CalcoloRatingImpl;
+import scraping.Scraping;
+import scraping.ScrapingImpl;
 
 public class LogicsHomeImpl implements LogicsHome {
 
+	private List<Calciatore> li;
+	private String stagione;
+	
+	public LogicsHomeImpl(String stagione) { //di default
+		li=new ArrayList<>();
+		this.stagione=stagione;
+	}
+	
+	@Override
+	public String getStagione() {
+		return stagione;
+	}
+	
+	@Override
+	public void setStagione(String stagione) {
+		this.stagione=stagione;
+		
+	}
+	
 	@Override
 	public List<String> getStagioni() {
-		// TODO Auto-generated method stub
-		return null;
+		Scraping sc=new ScrapingImpl();
+		return sc.getStagioni();
 	}
 
 	@Override
 	public Boolean loadStagione(String stagione) {
-		// TODO Auto-generated method stub
-		return null;
+		ManageData md=new ManageDataImpl(stagione);
+		this.stagione=stagione;
+		try {
+			md.LoadData();
+		} catch (ClassNotFoundException | IOException e) {
+			return false;
+		}
+		this.li=md.getLi();
+		return true;
 	}
 
 	@Override
 	public Boolean downloadStagione(String stagione) {
-		// TODO Auto-generated method stub
-		return null;
+		ManageData md=new ManageDataImpl(stagione);
+		this.stagione=stagione;
+		try {
+			md.DownloadData();
+		} catch (ClassNotFoundException | IOException e) {
+			return false;
+		}
+		this.li=md.getLi();
+		return true;
 	}
 
 	@Override
-	public List<Calciatore> geLi() {
-		// TODO Auto-generated method stub
+	public List<Calciatore> getLi() throws FileNotFoundException, ClassNotFoundException, IOException {
+		CalcoloRating rat=new CalcoloRatingImpl(this.li);
+		rat.
 		return null;
 	}
+
+	
+
+	
 	
 }
