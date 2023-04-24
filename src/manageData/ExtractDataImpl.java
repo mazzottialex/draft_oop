8bd67ec3f5e7433ea52cccea32c8ff62e19d0a1d
martino <martino.narducci@studio.unibo.it>
2023-04-24 11:19:17 +0200

    get titolari and get riserve in ExtractDataImpl

--- src/manageData/ExtractDataImpl.java
+++ src/manageData/ExtractDataImpl.java
@@ -2,6 +2,7 @@ package manageData;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -10,10 +11,14 @@ import java.util.Set;
 import java.util.function.Function;
 
 import data.Calciatore;
+import data.Modulo;
+import rating.CalcoloRating;
+import rating.CalcoloRatingImpl;
 
 public class ExtractDataImpl implements ExtractData{
 
 	private List<Calciatore> li;
+	private CalcoloRating r = new CalcoloRatingImpl(li);
 	
 	public ExtractDataImpl(List<Calciatore> li) throws FileNotFoundException, IOException, ClassNotFoundException {
 		this.li=li;
@@ -76,6 +81,88 @@ public class ExtractDataImpl implements ExtractData{
 						return 10000;})
 				.max((c1,c2)-> c1-c2)
 				.orElse(0);
+	}
+	
+	public List<Calciatore> getTitolariBySquadraByRuolo(String squadra, String ruolo, Modulo modulo) {
+		List<Calciatore> lista = getCalciatoreBySquadra(squadra).stream()
+				.filter(c -> c.getRuolo().equals(ruolo))
+				.sorted((c1, c2) -> r.getRating(c2).getX() - r.getRating(c1).getX())
+				.toList();
+		List<Calciatore> listaTitolari = new ArrayList<>();
+		int n = 0;
+		switch (ruolo) {
+		case "P":
+			n = 1;
+			listaTitolari = lista.stream()
+					.limit(n)
+					.toList();
+			break;
+		case "D":
+			n = modulo.getNumDif();
+			listaTitolari = lista.stream()
+					.limit(n)
+					.toList();
+			break;
+		case "C":
+			n = modulo.getNumCen();
+			listaTitolari = lista.stream()
+					.limit(n)
+					.toList();
+			break;
+		case "A":
+			n = modulo.getNumAtt();
+			listaTitolari = lista.stream()
+					.limit(n)
+					.toList();
+			break;
+		default:
+			break;
+		}
+		return listaTitolari;
+	}
+	
+	public List<Calciatore> getRiserveBySquadraByRuolo(String squadra, String ruolo, Modulo modulo) {
+		List<Calciatore> lista = getCalciatoreBySquadra(squadra).stream()
+				.filter(c -> c.getRuolo().equals(ruolo))
+				.sorted((c1, c2) -> r.getRating(c2).getX() - r.getRating(c1).getX())
+				.toList();
+		List<Calciatore> listaRiserve = new ArrayList<>();
+		int n = 2;
+		int m = 0;
+		switch (ruolo) {
+		case "P":
+			m = 1;
+			n = 1;
+			listaRiserve = lista.stream()
+					.skip(m)
+					.limit(n)
+					.toList();
+			break;
+		case "D":
+			m = modulo.getNumDif();
+			listaRiserve = lista.stream()
+					.skip(m)
+					.limit(n)
+					.toList();
+			break;
+		case "C":
+			m = modulo.getNumCen();
+			listaRiserve = lista.stream()
+					.skip(m)
+					.limit(n)
+					.toList();
+			break;
+		case "A":
+			m = modulo.getNumAtt();
+			listaRiserve = lista.stream()
+					.skip(m)
+					.limit(n)
+					.toList();
+			break;
+		default:
+			break;
+		}
+		return listaRiserve;
 	}
 
 }
