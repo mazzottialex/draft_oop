90a641afde3b86fab03b315f699c035c08d5d977
Martino Narducci <martino.narducci@studio.unibo.it>
2023-04-26 14:13:20 +0200

    aggiornamento Launch

--- src/test/Launch.java
+++ src/test/Launch.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.util.List;
 
 import data.Calciatore;
+import data.Modulo;
 import gui.HomeImpl;
+import logics.LogicsHome;
+import logics.LogicsHomeImpl;
 import gui.Home;
 import manageData.ExtractData;
 import manageData.ExtractDataImpl;
@@ -20,17 +23,24 @@ import scraping.ScrapingImpl;
 public class Launch {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		//Scraping sc=new ScrapingImpl();
-		//ManageData md=new ManageDataImpl(sc.getStagioni().get(3));
-		//ManageData md=new ManageDataImpl("2022-2023");
-		//System.out.print(sc.getStagioni().get(3));
-		//md.LoadData();
-		//List<Calciatore> li=md.getLi();
-		//ExtractData ex=new ExtractDataImpl(li);
-		//CalcoloRating ra=new CalcoloRatingImpl(li);
-		//ex.getLi().stream().sorted((c1,c2)->ra.getRating(c1).getX()-ra.getRating(c2).getX()).forEach(c -> System.out.println(c.getSquadra()+" "+ c.getNominativo()+"     \t RAT:"+ra.getRating(c).getX()+" \t A: "+ra.getRating(c).getY().getX()+" \t C: "+ra.getRating(c).getY().getY()+" \t D: "+ra.getRating(c).getY().getZ()+" \t Amm: "+ra.getRatingCartellino(c).getX()+" \t Esp: "+ra.getRatingCartellino(c).getY()));
+//		Scraping sc = new ScrapingImpl();
+//		ManageData md = new ManageDataImpl(sc.getStagioni().get(3));
+		ManageData md = new ManageDataImpl("2022-2023");
+//		System.out.print(sc.getStagioni().get(3));
+		md.LoadData();
+		List<Calciatore> li = md.getLi();
+		CalcoloRating r = new CalcoloRatingImpl(li);
+		li = r.updateRating();
+		ExtractData ex = new ExtractDataImpl(li);
+//		ex.getLi()
+//			.stream()
+//			.sorted((c1,c2)->ra.getRating(c1).getX()-ra.getRating(c2).getX())
+//			.forEach(c -> System.out.println(c.getSquadra()+" "+ c.getNominativo()+"     \t RAT:"+ra.getRating(c).getX()+" \t A: "+ra.getRating(c).getY().getX()+" \t C: "+ra.getRating(c).getY().getY()+" \t D: "+ra.getRating(c).getY().getZ()+" \t Amm: "+ra.getRatingCartellino(c).getX()+" \t Esp: "+ra.getRatingCartellino(c).getY()));
 
-		Home gui = new HomeImpl();
-		gui.setVisible(true);
+//		Home gui = new HomeImpl();
+//		gui.setVisible(true);
+
+		System.out.println(ex.getTitolariBySquadraByRuolo("NAP", "P", Modulo.M352));
+//		System.out.println(ex.getCalciatoreBySquadra("NAP"));
 	}
 }
