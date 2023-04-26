9a6720ee437c96663ca29f51c19c8e6981c42a8f
admin <admin@192.168.243.138>
2023-04-26 12:13:51 +0200

    aggiornamento ExtractDataImpl

--- src/manageData/ExtractDataImpl.java
+++ src/manageData/ExtractDataImpl.java
@@ -12,13 +12,10 @@ import java.util.function.Function;
 
 import data.Calciatore;
 import data.Modulo;
-import rating.CalcoloRating;
-import rating.CalcoloRatingImpl;
 
 public class ExtractDataImpl implements ExtractData{
 
 	private List<Calciatore> li;
-//	private CalcoloRating r = new CalcoloRatingImpl(li);
 	
 	public ExtractDataImpl(List<Calciatore> li) throws FileNotFoundException, IOException, ClassNotFoundException {
 		this.li = li;
@@ -27,7 +24,7 @@ public class ExtractDataImpl implements ExtractData{
 	@Override
 	public List<Calciatore> getCalciatoreBySquadra(String squadra) {
 		return li.stream()
-				.filter(c->c.getSquadra().equals(squadra))
+				.filter(c -> c.getSquadra().equals(squadra))
 				.toList();
 	}
 
@@ -39,7 +36,7 @@ public class ExtractDataImpl implements ExtractData{
 	@Override
 	public Optional<Calciatore> getCalciatoreByName(String name) {
 		return li.stream()
-				.filter(c->c.getNominativo().equals(name))
+				.filter(c -> c.getNominativo().equals(name))
 				.findFirst();
 	}
 
@@ -47,7 +44,7 @@ public class ExtractDataImpl implements ExtractData{
 	@Override
 	public List<Calciatore> getListaByRuolo(String ruolo) {
 		return li.stream()
-				.filter(c->c.getRuolo().equals(ruolo))
+				.filter(c -> c.getRuolo().equals(ruolo))
 				.toList();
 	}
 	
@@ -91,88 +88,55 @@ public class ExtractDataImpl implements ExtractData{
 	
 	@Override
 	public List<Calciatore> getTitolariBySquadraByRuolo(String squadra, String ruolo, Modulo modulo) {
-		List<Calciatore> lista = getCalciatoreBySquadra(squadra).stream()
-				.filter(c -> c.getRuolo().equals(ruolo))
-				.sorted((c1, c2) -> c2.getRating().getX() - c1.getRating().getX())
-//				.sorted((c1, c2) -> r.getRating(c2).getX() - r.getRating(c1).getX())
-				.toList();
-		List<Calciatore> listaTitolari = new ArrayList<>();
-		int n = 0;
+		int n = 1;
 		switch (ruolo) {
-		case "P":
-			n = 1;
-			listaTitolari = lista.stream()
-					.limit(n)
-					.toList();
-			break;
 		case "D":
 			n = modulo.getNumDif();
-			listaTitolari = lista.stream()
-					.limit(n)
-					.toList();
 			break;
 		case "C":
 			n = modulo.getNumCen();
-			listaTitolari = lista.stream()
-					.limit(n)
-					.toList();
 			break;
 		case "A":
 			n = modulo.getNumAtt();
-			listaTitolari = lista.stream()
-					.limit(n)
-					.toList();
 			break;
-		default:
+		default:	//qui compreso il caso "P", perché n = 1 per il ruolo portiere
 			break;
 		}
-		return listaTitolari;
+		List<Calciatore> lista = getCalciatoreBySquadra(squadra).stream()
+				.filter(c -> c.getRuolo().equals(ruolo))
+				.sorted((c1, c2) -> c2.getRating().getX() - c1.getRating().getX())
+				.limit(n)
+				.toList();
+		return lista;
 	}
 	
 	@Override
 	public List<Calciatore> getRiserveBySquadraByRuolo(String squadra, String ruolo, Modulo modulo) {
-		List<Calciatore> lista = getCalciatoreBySquadra(squadra).stream()
-				.filter(c -> c.getRuolo().equals(ruolo))
-				.sorted((c1, c2) -> c2.getRating().getX() - c1.getRating().getX())
-//				.sorted((c1, c2) -> r.getRating(c2).getX() - r.getRating(c1).getX())
-				.toList();
-		List<Calciatore> listaRiserve = new ArrayList<>();
 		int n = 2;
-		int m = 0;
+		int m = 1;
 		switch (ruolo) {
 		case "P":
-			m = 1;
 			n = 1;
-			listaRiserve = lista.stream()
-					.skip(m)
-					.limit(n)
-					.toList();
 			break;
 		case "D":
 			m = modulo.getNumDif();
-			listaRiserve = lista.stream()
-					.skip(m)
-					.limit(n)
-					.toList();
 			break;
 		case "C":
 			m = modulo.getNumCen();
-			listaRiserve = lista.stream()
-					.skip(m)
-					.limit(n)
-					.toList();
 			break;
 		case "A":
 			m = modulo.getNumAtt();
-			listaRiserve = lista.stream()
-					.skip(m)
-					.limit(n)
-					.toList();
 			break;
 		default:
 			break;
 		}
-		return listaRiserve;
+		List<Calciatore> lista = getCalciatoreBySquadra(squadra).stream()
+				.filter(c -> c.getRuolo().equals(ruolo))
+				.sorted((c1, c2) -> c2.getRating().getX() - c1.getRating().getX())
+				.skip(m)
+				.limit(n)
+				.toList();
+		return lista;
 	}
 
 	@Override
