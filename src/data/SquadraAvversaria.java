49b8235d335660d43f85a678624fd359250a219c
admin <admin@192.168.243.138>
2023-04-26 11:07:50 +0200

    aggiornamento SquadraAvversaria

--- src/data/SquadraAvversaria.java
+++ src/data/SquadraAvversaria.java
@@ -29,29 +29,4 @@ public class SquadraAvversaria{
 	public Modulo getModulo() {
 		return modulo;
 	}
-	/*
-	public List<Calciatore> getTitolari() {
-		List<Calciatore> liP = getTitolariBySquadraByRuolo(nomeSquadra, "P", modulo);
-		List<Calciatore> liD = getTitolariBySquadraByRuolo(nomeSquadra, "D", modulo);
-		List<Calciatore> liC = getTitolariBySquadraByRuolo(nomeSquadra, "C", modulo);
-		List<Calciatore> liA = getTitolariBySquadraByRuolo(nomeSquadra, "A", modulo);
-		titolari.addAll(liP);
-		titolari.addAll(liD);
-		titolari.addAll(liC);
-		titolari.addAll(liA);
-		return titolari;
-	}
-	
-	public List<Calciatore> getRiserve() {
-		List<Calciatore> liP = getRiserveBySquadraByRuolo(nomeSquadra, "P", modulo);
-		List<Calciatore> liD = getRiserveBySquadraByRuolo(nomeSquadra, "D", modulo);
-		List<Calciatore> liC = getRiserveBySquadraByRuolo(nomeSquadra, "C", modulo);
-		List<Calciatore> liA = getRiserveBySquadraByRuolo(nomeSquadra, "A", modulo);
-		riserve.addAll(liP);
-		riserve.addAll(liD);
-		riserve.addAll(liC);
-		riserve.addAll(liA);
-		return riserve;
-	}
-	*/
 }
