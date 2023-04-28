7980543a1e4be7cef2c2c776af90f94363054e03
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-04-28 10:15:43 +0200

    Flag nel main, per testing

--- src/test/Launch.java
+++ src/test/Launch.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.List;
 
 import data.Calciatore;
+import data.Modulo;
 import gui.HomeImpl;
 import gui.Home;
 import manageData.ExtractData;
@@ -20,17 +21,30 @@ import scraping.ScrapingImpl;
 public class Launch {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		//Scraping sc=new ScrapingImpl();
-		//ManageData md=new ManageDataImpl(sc.getStagioni().get(3));
-		//ManageData md=new ManageDataImpl("2022-2023");
-		//System.out.print(sc.getStagioni().get(3));
-		//md.LoadData();
-		//List<Calciatore> li=md.getLi();
-		//ExtractData ex=new ExtractDataImpl(li);
-		//CalcoloRating ra=new CalcoloRatingImpl(li);
-		//ex.getLi().stream().sorted((c1,c2)->ra.getRating(c1).getX()-ra.getRating(c2).getX()).forEach(c -> System.out.println(c.getSquadra()+" "+ c.getNominativo()+"     \t RAT:"+ra.getRating(c).getX()+" \t A: "+ra.getRating(c).getY().getX()+" \t C: "+ra.getRating(c).getY().getY()+" \t D: "+ra.getRating(c).getY().getZ()+" \t Amm: "+ra.getRatingCartellino(c).getX()+" \t Esp: "+ra.getRatingCartellino(c).getY()));
-
-		Home gui = new HomeImpl();
-		gui.setVisible(true);
+		Boolean flagGui=true;
+		
+		if(flagGui) {
+			Home gui = new HomeImpl();
+			gui.setVisible(true);
+		}
+		else {
+			//Scraping sc=new ScrapingImpl();
+			//ManageData md=new ManageDataImpl(sc.getStagioni().get(3));
+			ManageData md=new ManageDataImpl("2022-2023");
+			//System.out.print(sc.getStagioni().get(3));
+			md.LoadData();
+			List<Calciatore> li=md.getLi();
+			CalcoloRating ra=new CalcoloRatingImpl(li);
+			li=ra.updateRating();
+			ExtractData ex=new ExtractDataImpl(li);
+			//System.out.println(ex.getCalciatoreByName("Kim Min-Jae"));
+			System.out.println(ex.getTitolariBySquadraByRuolo("NAP", "D", Modulo.M343));
+			//ex.getLi().stream().sorted((c1,c2)->ra.getRating(c1).getX()-ra.getRating(c2).getX()).forEach(c -> System.out.println(c.getSquadra()+" "+ c.getNominativo()+"     \t RAT:"+ra.getRating(c).getX()+" \t A: "+ra.getRating(c).getY().getX()+" \t C: "+ra.getRating(c).getY().getY()+" \t D: "+ra.getRating(c).getY().getZ()+" \t Amm: "+ra.getRatingCartellino(c).getX()+" \t Esp: "+ra.getRatingCartellino(c).getY()));
+		}
+		
+		
+		
+		
+		
 	}
 }
