fa89df883ced1d8dc1e8bd54ef4e2a7d9474a874
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-04-30 11:54:05 +0200

    Gui button e lbl stagione

--- src/gui/Archivio.java
+++ src/gui/Archivio.java
@@ -1,7 +1,11 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -19,6 +23,7 @@ import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
@@ -34,6 +39,13 @@ import manageData.ExtractDataImpl;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Archivio extends Base {
 	private JTable table;
@@ -71,11 +83,50 @@ public class Archivio extends Base {
 		li=ex.getListOrdered(c->-c.getRating().getX());
 		li.stream().forEach(c -> ((DefaultTableModel) tm).addRow(c.toVector()));
 		
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(600,40));
+		panel.setBackground(getForeground());
+		contentPane.add(panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{61, 40, 1, 0};
+		gbl_panel.rowHeights = new int[]{23, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JButton btnHome = new JButton("HOME");
+		btnHome.setFont(new Font("DejaVu Sans", Font.PLAIN, 12));
+		btnHome.setBackground(Color.white);
+		btnHome.setRolloverEnabled(true);
+		btnHome.setForeground(Color.BLUE);
+		GridBagConstraints gbc_btnHome = new GridBagConstraints();
+		gbc_btnHome.anchor = GridBagConstraints.WEST;
+		gbc_btnHome.insets = new Insets(0, 0, 0, 5);
+		gbc_btnHome.gridx = 0;
+		gbc_btnHome.gridy = 0;
+		panel.add(btnHome, gbc_btnHome);
+		
+		JLabel lblNewLabel = new JLabel("2022-2023");
+		lblNewLabel.setFont(new Font("DejaVu Sans", Font.PLAIN, 14));
+		lblNewLabel.setForeground(Color.white);
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 0;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		JLabel lblEmpty = new JLabel("-----------------");
+		lblEmpty.setForeground(getForeground());
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.gridx = 2;
+		gbc_lblNewLabel_1.gridy = 0;
+		panel.add(lblEmpty, gbc_lblNewLabel_1);
+		
 		
 		
 		table= new JTable(tm);
 		JScrollPane scrollPane=new JScrollPane(table);
-		scrollPane.setPreferredSize(new Dimension(620,610));
+		scrollPane.setPreferredSize(new Dimension(620,600));
 		contentPane.add(scrollPane);
 		
 		TableRowSorter<TableModel> sorter=new TableRowSorter<TableModel>(tm);
@@ -87,8 +138,6 @@ public class Archivio extends Base {
 		});
 		table.setRowSorter(sorter);
 		
-		JButton btnNewButton = new JButton("Torna alla home");
-		getContentPane().add(btnNewButton);
 	}
 
 }
