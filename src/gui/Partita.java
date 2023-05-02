c573702823d0491769967359697bd5bb0f2dae86
Martino Narducci <martino.narducci@studio.unibo.it>
2023-05-02 11:47:33 +0200

    modifiche a Partita

--- src/gui/Partita.java
+++ src/gui/Partita.java
@@ -24,8 +24,7 @@ import javax.swing.SwingWorker;
 public class Partita extends JPanel implements ActionListener, PropertyChangeListener{
 
 	private static final long serialVersionUID = 1353924410908490014L;
-	//JFrame f;
-	//JPanel panel;
+	
 	JProgressBar jpPb;
 	JLabel jlNomeSq1;
 	JLabel jlScoreSq1;
@@ -44,40 +43,86 @@ public class Partita extends JPanel implements ActionListener, PropertyChangeLis
 	 * Create the frame.
 	 */
 	
-	class Task extends SwingWorker<Void, Void> {
-        /*
-         * Main task. Executed in background thread.
-         */
-        @Override
-        public Void doInBackground() {
+	abstract class PausableSwingWorker<K, V> extends SwingWorker<K, V> {
+
+        private volatile boolean isPaused;
+        private volatile boolean isRunning = false;
+
+        public final void pause() {
+            if (!isPaused() && !isDone()) {
+                isPaused = true;
+                firePropertyChange("paused", false, true);
+            }
+        }
+
+        public final void resume() {
+            if (isPaused() && !isDone()) {
+                isPaused = false;
+                firePropertyChange("paused", true, false);
+            }
+        }
+        
+        public final void start() {
+            if (!isDone()) {
+            	isRunning = true;
+            }
+        }
+
+        public final boolean isPaused() {
+            return isPaused;
+        }
+        
+        public final boolean isRunning() {
+            return isRunning;
+        }
+    }
+	
+	class Task extends PausableSwingWorker<Void, Void> {
+
+		/*
+		 * Main task. Executed in background thread.
+		 */
+		@Override
+		public Void doInBackground() {
             int progress = 0;
             //Initialize progress property.
             setProgress(0);
             while (progress < 100) {
-                //Sleep for up to one second.
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ignore) {}
-                if (progress > 0) {
-					jbSubs.setEnabled(true);
-				}
-                //Make progress.
-                progress += 10;
-                setProgress(Math.min(progress, 100));
+            	if (!isPaused()) {
+            		//Sleep for up to one second.
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ignore) {}
+                    //Make progress.
+                    progress += 1;
+                    setProgress(Math.min(progress, 100));
+                    //Abilita bottone sostituzioni
+                    if (progress > 0) {
+    					jbSubs.setEnabled(true);
+    				}
+                    //Fine 1° tempo
+//                    if (progress == 50) {
+//						task.pause();
+//					}
+    			} else {
+    				try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException ignore) {}
+    			}
             }
             return null;
         }
-
-        /*
+		
+		/*
          * Executed in event dispatching thread
          */
         @Override
         public void done() {	//quando finisce
         	next.setEnabled(true);
-        	 JOptionPane.showMessageDialog(null, "Partita finita");	//dire che è finito
+        	JOptionPane.showMessageDialog(null, "Partita finita");	//dire che è finito
         }
-    }
-	
+	}
+
 	public Partita() {
 		// Define the panel to hold the components
 		super(new GridBagLayout());
@@ -100,108 +145,87 @@ public class Partita extends JPanel implements ActionListener, PropertyChangeLis
 		startStop.addActionListener(this);
 		jbSubs = new JButton("Subs");
 		jbSubs.setEnabled(false);
+		jbSubs.setActionCommand("subs");
+		jbSubs.addActionListener(this);
 		min = new JLabel("Minuto: 0°");
 		next = new JButton("Avanti");
 		next.setEnabled(false);
-		jpPb = new JProgressBar();					//non funziona
+		jpPb = new JProgressBar();
 		jpPb.setValue(0);
 		jpPb.setStringPainted(true);
+		jpPb.setString("Minuto 0°");
+		task = new Task();
+        task.addPropertyChangeListener(this);
 		
-
+        
         // Put constraints on different buttons
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 0;
-//        gbc.ipadx = 50;
-        //gbc.ipady = 10;
         /*panel.*/add(jlNomeSq1, gbc);
         
-        //gbc.fill = GridBagConstraints.VERTICAL;
         gbc.gridx = 1;
         gbc.gridy = 0;
-        //gbc.weighty = 1;
         gbc.ipadx = 50;
         /*panel.*/add(new JLabel("vs", SwingConstants.CENTER), gbc);
         
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 2;
         gbc.gridy = 0;
-//        gbc.ipadx = 50;
-        //gbc.ipady = 10;
         /*panel.*/add(jlNomeSq2, gbc);
                 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 2;
-//        gbc.ipadx = 50;
-        //gbc.ipady = 10;
         /*panel.*/add(jlScoreSq1, gbc);
                 
-        //gbc.fill = GridBagConstraints.VERTICAL;
         gbc.gridx = 1;
         gbc.gridy = 2;
         gbc.ipadx = 50;
-        //gbc.weighty = 1;
         /*panel.*/add(new JLabel("-", SwingConstants.CENTER), gbc);
                 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 2;
         gbc.gridy = 2;
-//        gbc.ipadx = 50;
-        //gbc.ipady = 10;
         /*panel.*/add(jlScoreSq2, gbc);
         
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 3;
-//        gbc.ipadx = 50;
         gbc.ipady = 200;
         /*panel.*/add(jlTabSq1, gbc);
                 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 2;
         gbc.gridy = 3;
-//        gbc.ipadx = 50;
         gbc.ipady = 200;
         /*panel.*/add(jlTabSq2, gbc);
        
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 4;
-        //gbc.weightx = 1;
         gbc.gridwidth = 3;
         gbc.ipady = 5;
         /*panel.*/add(jpPb, gbc);
         
-      //gbc.fill = GridBagConstraints.NONE;
+        JPanel southWest = new JPanel();
+        southWest.add(jbSubs);
         gbc.gridx = 0;
         gbc.gridy = 5;
-        //gbc.weightx = 1;
         gbc.gridwidth = 1;
         gbc.ipady = 5;
-        gbc.insets = new Insets(10, 20, 10, 10);
-        /*panel.*/add(min, gbc);
+        /*panel.*/add(southWest, gbc);
         
-        //gbc.fill = GridBagConstraints.NONE;
         gbc.gridx = 1;
         gbc.gridy = 5;
-        //gbc.weightx = 1;
         gbc.gridwidth = 1;
         gbc.ipady = 5;
-        gbc.insets = new Insets(10, 10, 10, 10);
         /*panel.*/add(startStop, gbc);
         
-        
         JPanel southEast = new JPanel();
-        //southEast.setLayout(new FlowLayout());
-        southEast.add(jbSubs);
         southEast.add(next);
-        
-        gbc.insets = new Insets(10, 0, 10, 0);
-        //gbc.fill = GridBagConstraints.NONE;
         gbc.gridx = 2;
         gbc.gridy = 5;
-        //gbc.weightx = 1;
         /*panel.*/add(southEast, gbc);
 	}
 	
@@ -209,12 +233,22 @@ public class Partita extends JPanel implements ActionListener, PropertyChangeLis
      * Invoked when the user presses the start button.
      */
     public void actionPerformed(ActionEvent evt) {
-        //startButton.setEnabled(false);
-        //Instances of javax.swing.SwingWorker are not reusuable, so
-        //we create new instances as needed.
-        task = new Task();
-        task.addPropertyChangeListener(this);
-        task.execute();
+        //Instances of javax.swing.SwingWorker are not reusuable, so we create new instances as needed.
+    	if (evt.getActionCommand() == "start") {
+    		if (!task.isRunning()) {
+    			task.execute();
+    			task.start();
+			} else {
+				if (task.isPaused()) {
+					task.resume();
+				} else {
+					task.pause();
+				}
+			}
+		} else if (evt.getActionCommand() == "subs") {
+			task.pause();
+			// TODO Aprire finestra per cambiare giocatori
+		}
     }
 
     /**
@@ -226,7 +260,7 @@ public class Partita extends JPanel implements ActionListener, PropertyChangeLis
             int progress = (Integer) evt.getNewValue();
             jpPb.setValue(progress);
             val = Math.round(((float)task.getProgress() / 100f) * 90f);
-            jpPb.setString(Integer.toString(val));
+            jpPb.setString("Minuto " + Integer.toString(val) + "°");
         } 
     }
     
@@ -235,9 +269,8 @@ public class Partita extends JPanel implements ActionListener, PropertyChangeLis
      * on the event-dispatching thread.
      */
     public void createAndShowGUI() {
-    	
     	//Create and set up the window.
-    	JFrame f = new JFrame("SIMULAZIONE -.................... PARTITA");
+    	JFrame f = new JFrame("SIMULAZIONE PARTITA");
     	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		final int sw = (int) screen.getWidth();
@@ -257,14 +290,14 @@ public class Partita extends JPanel implements ActionListener, PropertyChangeLis
 		f.setVisible(true);
     }
     
-//    public static void main(String[] args) {
-//        //Schedule a job for the event-dispatching thread:
-//        //creating and showing this application's GUI.
-//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                createAndShowGUI();
-//            }
-//        });
-//    }
-
+    /*
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }*/
 }
