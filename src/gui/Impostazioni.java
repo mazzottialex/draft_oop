d329a2da0e588eea571890b888cc304d61b5594d
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-05-03 17:40:10 +0200

    Selezione stemma

--- src/gui/Impostazioni.java
+++ src/gui/Impostazioni.java
@@ -3,14 +3,18 @@ package gui;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import utils.*;
 import javax.swing.JFrame;
@@ -58,6 +62,7 @@ public class Impostazioni extends Base {
 		lblNomeSquadra.setFont(new Font("DejaVu Sans", Font.PLAIN, 14));
 		gbc.gridx=0;
 		gbc.gridy=0;
+		gbc.gridwidth=1;
 		gbc.insets=new Insets(10, 5, 10, 5);
 		contentPane.add(lblNomeSquadra, gbc);
 		
@@ -75,34 +80,42 @@ public class Impostazioni extends Base {
 	    lblImg.setForeground(Color.white);
 	    lblImg.setFont(new Font("DejaVu Sans", Font.PLAIN, 14));
 		
+	    List<String> liStemmi=new ArrayList<>();
+	    liStemmi.add("res/stemmi/bianco.png");
+	    liStemmi.add("res/stemmi/blu.png");
+	    liStemmi.add("res/stemmi/giallo.png");
+	    liStemmi.add("res/stemmi/nero.png");
+	    liStemmi.add("res/stemmi/verde.png");
+	    liStemmi.add("res/stemmi/viola.png");
+	    liStemmi.add("res/stemmi/rosso.png");
 	    
+	    JPanel stemmi=new JPanel();
+		stemmi.setLayout(layout);
+	    for(int i=0;i<liStemmi.size();i++)
+	    {	   
+	    	JButton btnStemma=new JButton();
+			ImageIcon img=new ImageIcon(liStemmi.get(i));
+			Image image = img.getImage(); // transform it 
+			Image newimg = image.getScaledInstance(40, 45,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+			img = new ImageIcon(newimg);
+			btnStemma.setBackground(Color.white);
+			btnStemma.setIcon(img);
+			btnStemma.setBorderPainted(false);
+			gbc.gridx=i%3;
+			gbc.gridy=i/3;
+			gbc.insets=new Insets(2, 2, 2, 2);
+			stemmi.add(btnStemma,gbc);
+			
+	    };
+	    gbc.gridwidth=0;
+	    gbc.gridx=0;
+		gbc.gridy=2;
+	    contentPane.add(stemmi,gbc);
+
 	    btnAddImg.addActionListener(new ActionListener() {
 	         @Override
 	    	public void actionPerformed(ActionEvent e) {
-	        	 JFileChooser fileChooser = new JFileChooser();
-	        	 fileChooser.addChoosableFileFilter(new ImageFilter());
-	        	 fileChooser.setAcceptAllFileFilterUsed(false);
-
-	        	 int option = fileChooser.showOpenDialog(contentPane);
-	        	 if(option == JFileChooser.APPROVE_OPTION){
-	        		 File file = fileChooser.getSelectedFile();
-	        		 //lblImg.setText("File Selected: " + file.getName());
-	        		 BufferedImage img = null;
-	        		 try {
-	        		     img = ImageIO.read(file);
-	        		     JLabel lblImg = new JLabel(new ImageIcon(img.getScaledInstance(80, 80, java.awt.Image.SCALE_SMOOTH)));
-	        		     lblImg.setBorder(new RoundedBorder(Color.BLACK, 10));
-
-	        		     gbc.gridx=1;
-	        			 gbc.gridy=1;
-	        		     contentPane.add(lblImg,gbc);
-	        		     contentPane.updateUI();
-	        		 } catch (Exception e1) {
-	        			 e1.printStackTrace();
-	        		 }
-	        	 }else{
-	        		 lblImg.setText("Open command canceled");
-	             }
+	        	 
 	         }
 	    });
 	      
