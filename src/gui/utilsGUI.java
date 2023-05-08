132bb1d7ee2e411466ae21d4dbcf04d3a182de89
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-05-08 21:25:35 +0200

    Classe per metodo static getPanel

---
+++ src/gui/utilsGUI.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class utilsGUI {
+
+	public static JPanel getPanelCalciatore(String calciatore, String ruolo) {
+		String file="";
+		switch(ruolo) {
+		case "P": file="res/portiere.png";
+		case "D": file="res/difensore.png";
+		case "C": file="res/centrocampista.png";
+		case "A": file="res/attaccante.png";
+		}
+		
+		GridBagConstraints gbc=new GridBagConstraints();
+		GridBagLayout layout=new GridBagLayout();
+		
+		JPanel panelGiocatore=new JPanel();
+		panelGiocatore.setLayout(layout);
+		
+		JLabel lblNome=new JLabel("Osimenh");
+		lblNome.setFont(new Font("DejaVu Sans", Font.PLAIN, 14));
+		JLabel lblIcona = new JLabel();
+		ImageIcon img=new ImageIcon(file);
+		Image image = img.getImage(); // transform it 
+		Image newimg = image.getScaledInstance(90, 90,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way  
+		img = new ImageIcon(newimg);
+		lblIcona.setIcon(img);
+		gbc.gridy=0;
+		panelGiocatore.add(lblIcona,gbc);
+		gbc.gridy=1;
+		panelGiocatore.add(lblNome,gbc); 
+		return panelGiocatore;
+	}
+}
