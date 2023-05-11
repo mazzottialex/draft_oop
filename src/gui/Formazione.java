3f8bc48c96f0036699e9e68146bff859fc56ff8d
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-05-11 09:01:29 +0200

    Action Listener Scegli formazione

--- src/gui/Formazione.java
+++ src/gui/Formazione.java
@@ -2,6 +2,9 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -9,13 +12,16 @@ import java.util.Random;
 import java.util.Set;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import data.Calciatore;
 import data.Modulo;
 
 public class Formazione extends Base{
-	
+	private List<Modulo> liModuli;
 	public Formazione() {
 		JButton btnScegli=new JButton("Scegli Formazione");
 		btnScegli.setFont(new Font("DejaVu Sans", Font.PLAIN, 12));
@@ -24,7 +30,37 @@ public class Formazione extends Base{
 		btnScegli.setForeground(Color.BLUE);
 		Modulo moduli[]=Modulo.values();
 		List<Modulo> li=Arrays.asList(Modulo.values());
-		li=getRandomMod(li);
+		liModuli=getRandomMod(li);
+		btnScegli.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JButton btn= (JButton) e.getSource();
+				JPanel panel=(JPanel) btn.getParent();
+				JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(btn);
+
+				try {
+					DialogSceltaFormazione dialog=new DialogSceltaFormazione(parent, true, liModuli);
+					dialog.setVisible(true);
+					Modulo modulo=dialog.getModulo();
+					if(modulo!=null) {
+						panel.remove(btn);
+						JLabel lblModulo=new JLabel("Modulo Scelto:"+modulo.toString());
+						panel.add(lblModulo);
+					}
+				} catch (ClassNotFoundException | IOException e1) {
+					e1.printStackTrace();
+				}
+				panel.revalidate();
+				panel.repaint();
+				
+			}
+		});
+		contentPane.add(btnScegli);
+		
+		
+		
+		
 		
 	}
 	private List<Modulo> getRandomMod(List<Modulo> li){
