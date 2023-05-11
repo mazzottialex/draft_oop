bc0b2bf43fba763e604f1f137360a9bfb8bfea0e
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-05-11 08:52:11 +0200

    Moduli random

--- src/gui/Formazione.java
+++ src/gui/Formazione.java
@@ -2,9 +2,17 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import data.Calciatore;
+import data.Modulo;
 
 public class Formazione extends Base{
 	
@@ -14,6 +22,24 @@ public class Formazione extends Base{
 		btnScegli.setBackground(Color.white);
 		btnScegli.setRolloverEnabled(true);
 		btnScegli.setForeground(Color.BLUE);
+		Modulo moduli[]=Modulo.values();
+		List<Modulo> li=Arrays.asList(Modulo.values());
+		li=getRandomMod(li);
 		
+	}
+	private List<Modulo> getRandomMod(List<Modulo> li){
+		Random rnd = new Random();
+		Set<Integer> posizioni = new HashSet<>();
+		for(int i=0; i<5; i++) {
+			int num;
+			do {
+				num=rnd.nextInt(li.size());
+			}while(posizioni.contains(num));
+			posizioni.add(num);
+		}
+		
+		return posizioni.stream()
+				.map(p-> li.get(p))
+				.toList();
 	}
 }
