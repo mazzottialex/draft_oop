14a00564d4786e615cec6f8ab3b83895440f375d
martino <martino.narducci@studio.unibo.it>
2023-05-17 09:57:21 +0200

    creazione metodi da completare in LogicsPartitaImpl

--- src/logics/LogicsPartitaImpl.java
+++ src/logics/LogicsPartitaImpl.java
@@ -1,15 +1,31 @@
 package logics;
 
+import java.util.Map;
+
 import data.SquadraAvversaria;
 import gui.Partita;
 
 public class LogicsPartitaImpl implements LogicsPartita{
 	private SquadraAvversaria s1;
 	private SquadraAvversaria s2;
+	private double gol1;
+	private double gol2;
 	
 	public LogicsPartitaImpl(SquadraAvversaria s1, SquadraAvversaria s2) {
 		super();
 		this.s1 = s1;
 		this.s2 = s2;
+		this.gol1 = 0;
+		this.gol2 = 0;
+	}
+	
+	public Map<String, Double> makeResult(Map<String, Double> risultato) {
+		// TODO
+//		gol1 = risultato.get(s1);
+		return risultato;
+	}
+	
+	public void addGol() {
+		//TODO
 	}
 }
