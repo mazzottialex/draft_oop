f36656e3cdebcc6c35bd1bdc4b3b48af6b3da1fd
alex.mazzotti <alex.mazzotti2@studio.unibo.it>
2023-05-17 10:31:53 +0200

    Aggiunti panchinari

--- src/gui/Draft.java
+++ src/gui/Draft.java
@@ -30,6 +30,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,8 +41,8 @@ import javax.swing.SwingUtilities;
 
 public class Draft extends Base {
 
-	private List<Calciatore> liRuolo;
-	private final Modulo mod;
+	private List<Calciatore> liTitolari;
+	private List<Calciatore> liPanchina; 
 	private final List<String> ruoli=List.of("A","C","D","P");
 	private final int nDraft=5;
 	private List<Calciatore> liCalciatori=new ArrayList<>();
@@ -52,12 +54,13 @@ public class Draft extends Base {
 		contentPane.setLayout(layout);
 		
 		List<JButton> liBtn=new ArrayList<>();
-		this.mod=mod;
 
 		ExtractData ex;
 		try {
 			ex = new ExtractDataImpl(li);
-			liRuolo=ex.getRandom(mod.getNumAtt()*nDraft, mod.getNumCen()*nDraft, mod.getNumDif()*nDraft, nDraft); 
+			liTitolari=ex.getRandom(mod.getNumAtt()*nDraft, mod.getNumCen()*nDraft, mod.getNumDif()*nDraft, nDraft);
+			liPanchina=ex.getRandom(2*nDraft, 2*nDraft, 2*nDraft, nDraft);
+			Collections.reverse(liPanchina);
 		} catch (ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		}
@@ -67,6 +70,7 @@ public class Draft extends Base {
 		JButton btnProsegui=new JButton("Prosegui");
 		btnProsegui.setVisible(false);
 		
+		//titolari
 		for(int i=0; i<ruoli.size();i++) {
 			panelPosizione = new JPanel();
 			for(int j=0;j<mod.getN(ruoli.get(i));j++) {
@@ -86,7 +90,7 @@ public class Draft extends Base {
 						int index=liBtn.indexOf(btn);
 						DialogScelta dialog;
 						try {
-							dialog = new DialogScelta(parent, true, liRuolo.subList(5*index, 5*(index+1)), liRuolo.subList(5*index, 5*(index+1)).get(0).getRuolo());
+							dialog = new DialogScelta(parent, true, liTitolari.subList(5*index, 5*(index+1)), liTitolari.subList(5*index, 5*(index+1)).get(0).getRuolo());
 							dialog.setVisible(true);
 							Calciatore c=dialog.getCalciatore();
 							if(c!=null) {
@@ -111,8 +115,55 @@ public class Draft extends Base {
 			contentPane.add(panelPosizione, gbc);
 		}
 		
+		//panchinari
+		
+			panelPosizione = new JPanel();
+			for(int j=0;j<7;j++) {
+				panelPosizione.setLayout(layout);
+				panelGiocatore=new JPanel();
+				panelGiocatore.setLayout(layout);
+				JButton btnScegli=new JButton("Scegli");
+				btnScegli.setPreferredSize(new Dimension(100,50));
+				
+				liBtn.add(btnScegli);
+				btnScegli.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						JButton btn= (JButton) e.getSource();
+						JPanel panel=(JPanel) btn.getParent();
+		                JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(btn);
+						int index=liBtn.indexOf(btn);
+						DialogScelta dialog;
+						try {
+							dialog = new DialogScelta(parent, true, liTitolari.subList(5*index, 5*(index+1)), liTitolari.subList(5*index, 5*(index+1)).get(0).getRuolo());
+							dialog.setVisible(true);
+							Calciatore c=dialog.getCalciatore();
+							if(c!=null) {
+								panel.remove(btn);
+								liCalciatori.add(c);
+								panel.add(utilsGUI.getPanelCalciatore(c.getNominativo(), c.getRating().getX(), c.getRuolo()));
+								if(liCalciatori.size()>=11)
+									btnProsegui.setVisible(true);
+							}
+						} catch (ClassNotFoundException | IOException e1) {
+							e1.printStackTrace();
+						}
+						panel.revalidate();
+						panel.repaint();
+					}
+				});
+				panelGiocatore.add(btnScegli);
+				panelPosizione.add(panelGiocatore);
+			
+			gbc.gridy=4;
+			System.out.print(gbc.gridy);
+			contentPane.add(panelPosizione, gbc);
+		}
+		
+		
+		
 		gbc.insets=new Insets(10, 0, 0, 0);
-		gbc.gridy=4;
+		gbc.gridy=5;
 		contentPane.add(btnProsegui, gbc);
 		btnProsegui.setFont(new Font("DejaVu Sans", Font.PLAIN, 14));
 		btnProsegui.setBackground(Color.white);
