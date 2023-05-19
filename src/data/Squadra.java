fecbc31378a7ea197b704d5eec3481e5d20334e9
Davide Braccini <davide.braccini@studio.unibo.it>
2023-05-19 09:04:07 +0200

    Aggiunto campi titolari e riserve in Squadra.java e anche i loro getter

--- src/data/Squadra.java
+++ src/data/Squadra.java
@@ -1,14 +1,35 @@
 package data;
 
 import java.awt.Image;
+import java.util.List;
 
 public class Squadra {
 	public final String nomeSquadra;
 	public final String stemma;
+	private final List<Calciatore> titolari;
+	private final List<Calciatore> riserve;
 	
-	public Squadra(String nomeSquadra, String stemma) {
+	public Squadra(String nomeSquadra, String stemma, List<Calciatore> titolari, List<Calciatore> riserve) {
 		this.nomeSquadra=nomeSquadra;
 		this.stemma=stemma;
+		this.titolari = titolari;
+		this.riserve = riserve;
+	}
+
+	public String getNomeSquadra() {
+		return this.nomeSquadra;
+	}
+
+	public String getStemma() {
+		return this.stemma;
+	}
+
+	public List<Calciatore> getTitolari() {
+		return this.titolari;
+	}
+
+	public List<Calciatore> getRiserve() {
+		return this.riserve;
 	}
 
 	@Override
