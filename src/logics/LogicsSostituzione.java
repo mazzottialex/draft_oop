2032837883d05408b33cef3e006019880e398790
martino <martino.narducci@studio.unibo.it>
2023-05-29 10:50:34 +0200

    modifica in LogicsSostituzione

--- src/logics/LogicsSostituzione.java
+++ src/logics/LogicsSostituzione.java
@@ -9,7 +9,7 @@ import data.Calciatore;
 public interface LogicsSostituzione {
 	public List<Calciatore> getTitolari();
 	public List<Calciatore> getRiserve();
-	public void selzTit(Calciatore c);
+	public void selezTit(Calciatore c);
 	public void selezRis(Calciatore c);
 	public void sub(Container parent1, Container parent2, Component component1, Component component2);
 }
