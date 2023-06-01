9f6af4fa9c1fbd133038fde1c193be9690301d46
martino <martino.narducci@studio.unibo.it>
2023-06-01 11:51:03 +0200

    aggiunta javadoc

--- app/src/main/java/data/SquadraAvversaria.java
+++ app/src/main/java/data/SquadraAvversaria.java
@@ -1,15 +1,20 @@
 package data;
-import java.util.Random;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
+import java.util.Random;
 import manageData.ExtractData;
 import manageData.ExtractDataImpl;
 
-public class SquadraAvversaria implements Squadra{
+/** 
+ * Questa classe rappresenta un oggetto SquadraAvversaria ed implementa Squadra.
+ * Ogni squadra avversaria ha un id, un nome, uno stemma, un modulo, una lista di titolari,
+ * una lista di riserve e una lista di tutti i calciatori di una certa annata.
+ *
+ */
+public class SquadraAvversaria implements Squadra {
 	private int id;
 	private String nomeSquadra;
 	private final String stemma;
@@ -17,9 +22,21 @@ public class SquadraAvversaria implements Squadra{
 	private List<Calciatore> liTitolari = new ArrayList<>();
 	private List<Calciatore> liRiserve = new ArrayList<>();
 	private List<Calciatore> liCalciatori;
-	
-	
-	public SquadraAvversaria(int id, String nomeSquadra, Modulo modulo, List<Calciatore> li) throws FileNotFoundException, IOException, ClassNotFoundException {
+
+
+	/**
+	 * Costruttore della classe SquadraAvversaria.
+	 * 
+	 * @param id			L'id della squadra.
+	 * @param nomeSquadra	Il nome della squadra.
+	 * @param modulo		Il modulo della squadra.
+	 * @param li			La lista di calciatori di una certa annata.
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public SquadraAvversaria(int id, String nomeSquadra, Modulo modulo, List<Calciatore> li)
+			throws FileNotFoundException, IOException, ClassNotFoundException {
 		this.id = id;
 		this.nomeSquadra = nomeSquadra;
 		this.modulo = modulo;
@@ -27,16 +44,18 @@ public class SquadraAvversaria implements Squadra{
 		ExtractData ed = new ExtractDataImpl(li);
 		this.liTitolari = ed.getTitolari(nomeSquadra, modulo);
 		this.liRiserve = ed.getRiserve(nomeSquadra, modulo);
-		this.stemma=this.setStemma();
+		this.stemma = this.setStemma();
 	}
 
 	private String setStemma() {
-		final List<String> liStemmi=List.of("arancione.png", "azzurro.png", "bianco.png", "blu.png", "giallo.png", "nero.png", "rosso.png", "verde.png", "viola.png");
+		final List<String> liStemmi = List.of("arancione.png", "azzurro.png",
+				"bianco.png", "blu.png","giallo.png", "nero.png", "rosso.png",
+				"verde.png", "viola.png");
 		Random rnd = new Random();
-        int pos = rnd.nextInt(liStemmi.size());
-        return liStemmi.get(pos);
+		int pos = rnd.nextInt(liStemmi.size());
+		return liStemmi.get(pos);
 	}
-	
+
 	@Override
 	public int getId() {
 		return id;
@@ -51,16 +70,16 @@ public class SquadraAvversaria implements Squadra{
 	public Modulo getModulo() {
 		return modulo;
 	}
-	
+
 	@Override
 	public List<Calciatore> getTitolari(){
 		return liTitolari;
 	}
-	
+
 	@Override
 	public List<Calciatore> getTitolariDesc() {
 		return liTitolari.stream()
-				.sorted((c1,c2)->c1.getRuolo().compareTo(c2.getRuolo()))
+				.sorted((c1, c2) -> c1.getRuolo().compareTo(c2.getRuolo()))
 				.toList();
 	}
 
@@ -68,7 +87,7 @@ public class SquadraAvversaria implements Squadra{
 	public List<Calciatore> getRiserve() {
 		return liRiserve;
 	}
-	
+
 	@Override
 	public void setTitolari(List<Calciatore> liTitolari) {
 		this.liTitolari = liTitolari;
@@ -78,12 +97,12 @@ public class SquadraAvversaria implements Squadra{
 	public void setRiserve(List<Calciatore> liRiserve) {
 		this.liRiserve = liRiserve;
 	}
-	
+
 	@Override
 	public List<Calciatore> getLiCalciatori() {
 		return liCalciatori;
 	}
-	
+
 	@Override
 	public String getStemma() {
 		return this.stemma;
@@ -93,11 +112,11 @@ public class SquadraAvversaria implements Squadra{
 	public int getValutazione() {
 		return (int) Math.floor(
 				liTitolari.stream()
-						.map(c->c.getRating()
+				.map(c -> c.getRating()
 						.getX())
-						.mapToDouble(c->c)
-						.average()
-						.orElse(0));
+				.mapToDouble(c -> c)
+				.average()
+				.orElse(0));
 	}
 
 	@Override
@@ -121,5 +140,5 @@ public class SquadraAvversaria implements Squadra{
 		}
 		return portiere;
 	}
-	
+
 }
