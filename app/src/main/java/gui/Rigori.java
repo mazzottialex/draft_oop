2159b7fa3ab067a538f2a4cec659640d4f320548
martino <martino.narducci@studio.unibo.it>
2023-06-03 16:30:28 +0200

    aggiunto metodo e modificato un altro

--- app/src/main/java/gui/Rigori.java
+++ app/src/main/java/gui/Rigori.java
@@ -36,19 +36,17 @@ public class Rigori extends Base {
     private Squadra winner;
     private JButton chiudi;
     private LogicsRigori logics;
-//    private Iterator<Calciatore> tiratori1;
-//    private Iterator<Calciatore> tiratori2;
-//    private Iterator<String> ris1;
-//    private Iterator<String> ris2;
     private Map<Integer, Pair<Calciatore, String>> rig1;
     private Map<Integer, Pair<Calciatore, String>> rig2;
     private JPanel panel;
     private String str1;
     private String str2;
+	private Partita partita;
 
 	public Rigori(Squadra s1, Squadra s2, Partita partita) {
     	this.s1 = s1;
         this.s2 = s2;
+        this.partita = partita;
 		gol1 = 0;
 		gol2 = 0;
 		tiri1 = 0;
@@ -56,18 +54,10 @@ public class Rigori extends Base {
 		
 		totTiri = 0;
 		logics = new LogicsRigoriImpl(s1, s2);
-//		tiratori1 = logics.compute_old().get(0).keySet().iterator();
-//		tiratori2 = logics.compute_old().get(1).keySet().iterator();
-//		ris1 = logics.compute_old().get(0).values().iterator();
-//		ris2 = logics.compute_old().get(1).values().iterator();
 		rig1 = logics.compute().get(0);
 		rig2 = logics.compute().get(1);
 		str1 = "";
 		str2 = "";
-		
-		// gui di prova
-//        setLayout(new BorderLayout());
-//        setLocationRelativeTo(null);
         
         this.panel = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
@@ -89,28 +79,23 @@ public class Rigori extends Base {
         panel.add(new JLabel(s2.getNomeSquadra(), SwingConstants.LEFT), gbc);
 
         results1 = new JLabel();
-//        results1.setLayout(new BoxLayout(results1, BoxLayout.Y_AXIS));
-//        add(results1, BorderLayout.WEST);
         
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
         gbc.gridy = 2;
-        gbc.ipady = 200;
+//        gbc.ipady = 200;
         panel.add(results1, gbc);
 
         results2 = new JLabel();
-//        results2.setLayout(new BoxLayout(results2, BoxLayout.Y_AXIS));
-//        add(results2, BorderLayout.EAST);
                 
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 2;
         gbc.gridy = 2;
-        gbc.ipady = 200;
+//        gbc.ipady = 200;
         panel.add(results2, gbc);
 
         result = new JLabel();
-//        result.setHorizontalAlignment(JLabel.CENTER);
-//        add(result, BorderLayout.CENTER);
+        result.setHorizontalAlignment(SwingConstants.CENTER);
         
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = 0;
@@ -150,42 +135,12 @@ public class Rigori extends Base {
         TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
-//            	if (!tiratori1.hasNext() && !tiratori2.hasNext()) {
-//            		JOptionPane.showMessageDialog(null, "Tiri di rigore terminati. Squadra vincente: " + getNomeWinner());
-//            		result.setText(gol1 + " - " + gol2);
-//            		chiudi.setEnabled(true);
-//            		timer.cancel();
-//				} else {
-//					if (totTiri % 2 == 0) {
-//						if (tiratori1.hasNext() && ris1.hasNext()) {
-//							String res = ris1.next();
-//							String resultLabel = tiratori1.next().getNominativo() + ": " + res;
-//							str1 = str1 + resultLabel + "<br>";
-//							results1.setText("<html>" + str1 + "</html>");
-//							if (res.equals("Gol")) {
-//                            	gol1++;
-//                            }
-//							totTiri++;
-//						}
-//					} else {
-//						if (tiratori2.hasNext() && ris2.hasNext()) {
-//							String res = ris2.next();
-//							String resultLabel = tiratori2.next().getNominativo() + ": " + res;
-//							str2 = str2 + resultLabel + "<br>";
-//							results2.setText("<html>" + str2 + "</html>");
-//							if (res.equals("Gol")) {
-//                            	gol2++;
-//                            }
-//							totTiri++;
-//						}
-//					}
-//				}
-            	
-            	
             	if (!rig1.containsKey(tiri1) && !rig2.containsKey(tiri2)) {
-            		result.setText(gol1 + " - " + gol2);
-            		JOptionPane.showMessageDialog(null, "Tiri di rigore terminati. Squadra vincente: " + getNomeWinner());
             		chiudi.setEnabled(true);
+            		result.setText(gol1 + " - " + gol2);
+            		setWinner(gol1, gol2);
+            		partita.setWinnerR(winner, gol1, gol2);
+            		JOptionPane.showMessageDialog(null, "Tiri di rigore terminati. Squadra vincente: " + getNomeWinner());
             		timer.cancel();
 				} else {
 					if (totTiri % 2 == 0) {
@@ -222,16 +177,19 @@ public class Rigori extends Base {
     }
     
     private String getNomeWinner() {
-    	return getWinner().getNomeSquadra();
+    	return winner.getNomeSquadra();
 	}
 
 	public Squadra getWinner() {
+    	return winner;
+    }
+	
+	public void setWinner(int gol1, int gol2) {
 		if (gol1 > gol2) {
 			winner = s1;
 		} else {
 			winner = s2;
 		}
-    	return winner;
     }
     
     public void createAndShowGUI() {
