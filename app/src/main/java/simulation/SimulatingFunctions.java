abf30e6f988142626f64e7323892bdf32dddd6d8
martino <martino.narducci@studio.unibo.it>
2023-06-06 12:06:12 +0200

    refactoring + javadoc

--- app/src/main/java/simulation/SimulatingFunctions.java
+++ app/src/main/java/simulation/SimulatingFunctions.java
@@ -6,26 +6,94 @@ import java.util.Map;
 
 import data.Team;
 
+/**
+ * Interface {@code SimulatingFunctions} for simulating functions related to fantasy football teams.
+ */
 public interface SimulatingFunctions {
 
-    public int golSubitiFanta(Team s)
+	/**
+     * Retrieves the number of fantasy goals conceded by the specified team.
+     *
+     * @param t the team
+     * @return the number of fantasy goals conceded
+     * @throws FileNotFoundException    if the file is not found
+     * @throws ClassNotFoundException   if the class is not found
+     * @throws IOException              if an I/O error occurs
+     */
+    int getFantasyConcededGoals(Team t)
     		throws FileNotFoundException, ClassNotFoundException, IOException;
 
-    public int autogolFanta(Team s)
+    /**
+     * Retrieves the number of fantasy own goals scored by the specified team.
+     *
+     * @param t the team
+     * @return the number of fantasy own goals
+     * @throws FileNotFoundException    if the file is not found
+     * @throws ClassNotFoundException   if the class is not found
+     * @throws IOException              if an I/O error occurs
+     */
+    int getFantasyOwngoals(Team t)
     		throws FileNotFoundException, ClassNotFoundException, IOException;
 
-    public int rigoriParatiFanta(Team s)
+    /**
+     * Retrieves the number of fantasy saved penalties by the specified team.
+     *
+     * @param t the team
+     * @return the number of fantasy saved penalties
+     * @throws FileNotFoundException    if the file is not found
+     * @throws ClassNotFoundException   if the class is not found
+     * @throws IOException              if an I/O error occurs
+     */
+    int getFantasySavedPenalties(Team t)
     		throws FileNotFoundException, ClassNotFoundException, IOException;
 
-    public int differenzaRigoriFattiSbagliatiFanta(Team s)
+    /**
+     * Retrieves the difference between the number of scored and saved fantasy penalties by the specified team.
+     *
+     * @param t the team
+     * @return the difference between scored goals and saved penalties
+     * @throws FileNotFoundException    if the file is not found
+     * @throws ClassNotFoundException   if the class is not found
+     * @throws IOException              if an I/O error occurs
+     */
+    int getDeltaScoredSavedPenalties(Team t)
     		throws FileNotFoundException, ClassNotFoundException, IOException;
 
-    public double votoDifFanta(Team s, Map<String, Double> v)
+    /**
+     * Retrieves the fantasy defensive rating of the specified team based on the provided values.
+     *
+     * @param t the team
+     * @param v the values for calculating the rating
+     * @return the fantasy defensive rating
+     * @throws FileNotFoundException    if the file is not found
+     * @throws ClassNotFoundException   if the class is not found
+     * @throws IOException              if an I/O error occurs
+     */
+    double getFantasyDefensiveRating(Team t, Map<String, Double> v)
     		throws FileNotFoundException, ClassNotFoundException, IOException;
 
-    public double votoOffFanta(Team s, Map<String, Double> v)
+    /**
+     * Retrieves the fantasy offensive rating of the specified team based on the provided values.
+     *
+     * @param t the team
+     * @param v the values for calculating the rating
+     * @return the fantasy offensive rating
+     * @throws FileNotFoundException    if the file is not found
+     * @throws ClassNotFoundException   if the class is not found
+     * @throws IOException              if an I/O error occurs
+     */
+    double getFantasyOffensiveRating(Team t, Map<String, Double> v)
     		throws FileNotFoundException, ClassNotFoundException, IOException;
 
-    public int golFattiFanta(Team s)
+    /**
+     * Retrieves the number of fantasy goals scored by the specified team.
+     *
+     * @param t the team
+     * @return the number of fantasy goals scored
+     * @throws FileNotFoundException    if the file is not found
+     * @throws ClassNotFoundException   if the class is not found
+     * @throws IOException              if an I/O error occurs
+     */
+    int getFantasyScoredGoals(Team t)
     		throws FileNotFoundException, ClassNotFoundException, IOException;
 }
